70518b81b3a03f7fe5465a0d1c7ef7c014db2d7b
Tomas Dohnal <61649263+tomcany@users.noreply.github.com>
2021-07-03 12:14:15 +0200

    Add files via upload

---
+++ index-slides.js
@@ -0,0 +1,58 @@
+
+$(document).ready(function () {
+    sliderFitToScreenHeight()
+})
+
+$(window).resize(function () {
+    sliderFitToScreenHeight()
+})
+
+window.addEventListener("load", function() {
+    $("#index-slider-loading").fadeOut(300)
+}, false)
+
+function sliderFitToScreenHeight() {
+    var windowH = $(window).height()
+    var headerH = $(".main-header").height();
+    $("#index-slider").height(windowH - headerH);
+}
+
+$('#index-slider').on('slid.bs.carousel', function (e) {
+    if(e.to == 5){
+        IndexSliderDrawCharts()
+    }
+})
+
+function IndexSliderDrawCharts(){       
+    const options = {
+        borderWidth: 0,
+        events: [],
+        animation: {
+            duration: 3000
+        }
+    }         
+    new Chart(document.getElementById('consumersChart').getContext('2d'), {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+                data: [72, 28],
+                backgroundColor: [
+                '#f9ae28',
+                '#eff7f7'
+                ]
+            }]},
+        options: options
+    })
+    new Chart(document.getElementById('marketChart').getContext('2d'), {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+                data: [80, 20],
+                backgroundColor: [
+                '#f9ae28',
+                '#eff7f7'
+                ]
+            }]},
+        options: options
+    })
+}
